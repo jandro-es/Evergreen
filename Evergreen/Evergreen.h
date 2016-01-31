029d972d380aab81990629cd15451deccb0d8354
Alejandro Barros Cuetos <jandro@its-sistemas.com>
2016-01-31 15:05:51 +0000

    Initial Commit

---
+++ Evergreen/Evergreen.h
@@ -0,0 +1,19 @@
+//
+//  Evergreen.h
+//  Evergreen
+//
+//  Created by Alejandro Barros Cuetos on 31/01/2016.
+//  Copyright © 2016 Alejandro Barros Cuetos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Evergreen.
+FOUNDATION_EXPORT double EvergreenVersionNumber;
+
+//! Project version string for Evergreen.
+FOUNDATION_EXPORT const unsigned char EvergreenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Evergreen/PublicHeader.h>
+
+
